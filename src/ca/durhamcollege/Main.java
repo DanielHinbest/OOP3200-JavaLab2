134b84fe34ce2ca9b85721a4689b40836d4422a1
Daniel Hinbest <daniel.hinbest@dcmail.ca>
2020-11-20 16:39:33 -0500

    Making some progress

--- src/ca/durhamcollege/Main.java
+++ src/ca/durhamcollege/Main.java
@@ -5,6 +5,7 @@
  */
 package ca.durhamcollege;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 //driver class
@@ -12,23 +13,42 @@ public class Main {
 
     public static void main(String[] args)
     {
+        int ticketNumber = 0;
+        String clientID = null;
+        String dateInput = null;
+        LocalDate ticketDate = null;
+        String issueDescription = null;
+
         Scanner keyboard = new Scanner(System.in);
 
         // Default Constructor Display
         // We can use the mutators with this if we want
-        WorkTicket ticket = new WorkTicket();
+        WorkTicket ticket1 = new WorkTicket();
         System.out.println("Default Constructor\n_________________");
-        System.out.println(ticket.toString());
+        System.out.println(ticket1.toString());
 
-        // Using setWorkTicket is an option
+        // Using setWorkTicket to change ticket 1 data
+        System.out.println("Set Work Ticket\n_________________");
+        System.out.print("Enter the ticket number: ");
+        ticketNumber = keyboard.nextInt();
+        System.out.print("Enter the client ID: ");
+        clientID = keyboard.nextLine();
+        System.out.print("Enter the date (dd/mm/yyyy): ");
+        dateInput = keyboard.next();
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        ticketDate = LocalDate.parse(dateInput, dateFormat);
+        System.out.print("Enter the issue description: ");
+        issueDescription = keyboard.nextLine();
+        ticket1.setWorkTicket(ticketNumber, clientID, ticketDate, issueDescription);
+        System.out.println(ticket1.toString());
 
 
         // Parameterized constructor
         System.out.println("\nParameterized Constructor\n_________________");
-        WorkTicket ticket3 = new WorkTicket(3, "124", LocalDate.now(), "sdfs");
-        System.out.println("Work Ticket #:  " + ticket3.getTicketNumber());
-        System.out.println("Client Id:      " + ticket3.getClientID());
-        System.out.println("Date:           " + ticket3.getTicketDate());
-        System.out.println("Description     " + ticket3.getIssueDescription());
+        WorkTicket ticket2 = new WorkTicket(3, "124", LocalDate.now(), "sdfs");
+        System.out.println("Work Ticket #:  " + ticket2.getTicketNumber());
+        System.out.println("Client Id:      " + ticket2.getClientID());
+        System.out.println("Date:           " + ticket2.getTicketDate());
+        System.out.println("Description     " + ticket2.getIssueDescription());
     }
 }
